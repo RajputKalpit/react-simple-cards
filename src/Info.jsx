b1fb1645a207f51ffa138f120892937c8e33fe66
Kalpit Rajput <Kalpit.Rajput@investisdigital.com>
2023-11-01 12:06:22 +0000

    Make More Dynamically With Json Data

---
+++ src/Info.jsx
@@ -0,0 +1,18 @@
+let data = [{
+    "id": "1",
+    "image" : "https://media.licdn.com/dms/image/C4E03AQERvRfzqOoImw/profile-displayphoto-shrink_200_200/0/1645424985781?e=2147483647&v=beta&t=aihWOuo2eFKctESH6r3CmS8Z9tzSWJ2v4JViIkRK09k",
+    "name" : "Kalpit Rajput",
+    "intro" : "I am a frontend developer with 18 months of experience"
+  },{
+    "id": "2",
+    "image" : "https://media.licdn.com/dms/image/D5603AQF53u-2Tzvfnw/profile-displayphoto-shrink_200_200/0/1676011972212?e=2147483647&v=beta&t=Wma7BWkTfmO5sv_VaD-xxj3jaR6mZMqxm8Xa7GKSBek",
+    "name" : "Rahul Patel",
+    "intro" : "Rahul Patel a frontend developer with 18 months of experience"
+  },{
+    "id": "3",
+    "image" : "https://media.licdn.com/dms/image/D4D03AQG22xUPQNsjbw/profile-displayphoto-shrink_200_200/0/1683609735780?e=2147483647&v=beta&t=s0TW_KjTVv8gVGvcYLZZ_AEs3YBdD00crmYES8G46dY",
+    "name" : "Jenky Panchal",
+    "intro" : "Jainil Panchal a full stack developer with 5 years of experience"
+}]
+
+export default data;

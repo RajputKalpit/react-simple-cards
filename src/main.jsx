df51c739d8eec58fee7638ca070ef33b4a8ef8e4
Kalpit Rajput <Kalpit.Rajput@investisdigital.com>
2023-11-02 10:37:12 +0000

    Try To Use Swiper

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import data from './Info.jsx'
@@ -7,12 +7,12 @@ import Title from './Title.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
+  
     <Title title="Three Horizontal Cards" />
     <div className="card__row flex justify-evenly hover:justify-around">
     {data.map((item)=>{
       return <App key={item.id} img={item.image} name={item.name} intro={item.intro}/>
     })}
     </div>
-    <Title title="Slider Cards" />
   </>,
 )
